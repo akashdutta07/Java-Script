516228c1420a9e49425485b433e6facc2bddacf5
akashdutta07 <153263929+akashdutta07@users.noreply.github.com>
2025-03-07 14:24:36 +0000

    config done

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,19 @@
+const accountID = 144553
+// there r 2 ways of declaring variables, leta nd var
+//  but let is mostly used.
+let accountEmail = "akd.mail"
+var accountPass = "12345"
+accountCity = "Raipur"
+
+// accountID = 2 // this change is not allowed
+accountEmail = "akash.mail"
+accountPass = "54321"
+accountCity = "Bhilai"
+console.log(accountID);
+
+/* 
+prefer not to use var
+because of issue in block scope and functional scope
+*/
+
+console.table([accountID, accountEmail, accountPass, accountCity]);
